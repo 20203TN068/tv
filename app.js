0aafd57ab5e2ec912b564fcfe26764c11c2f408f
20203TN068 <89228281+20203TN068@users.noreply.github.com>
2023-06-06 23:18:22 -0600

    Cambios eliminados app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let powerOn = false;
 let silence = false;
+let volumenAnterior = 0;
 let settings = {
     volumenLevel: 0,
     channelLevel: 0
@@ -20,92 +21,92 @@ function updateButtons() {
     channelButton.innerText = `Canal: ${settings.channelLevel}`;
 }
 
+function printStatus() {
+    console.log(`Encendido: ${powerOn}`);
+    console.log(`Volumen: ${settings.volumenLevel}`);
+    console.log(`Canal: ${settings.channelLevel}`);
+    console.log(`Silencio: ${silence}`);
+}
+
 powerButton.addEventListener('click', () => {
     powerOn = !powerOn;
 
     if (powerOn) {
         powerButton.innerText = 'Apagar';
-        volumenButton.disabled = false;
-        channelButton.disabled = false;
-        moreVolumenLevelButton.disabled = false;
-        lessVolumenLevelButton.disabled = false;
-        moreChannelLevelButton.disabled = false;
-        lessChannelLevelButton.disabled = false;
-        silenceButton.disabled = false;
-    } else {
-        powerButton.innerText = 'Encender';
-        volumenButton.disabled = true;
-        channelButton.disabled = true;
-        moreVolumenLevelButton.disabled = true;
-        lessVolumenLevelButton.disabled = true;
-        moreChannelLevelButton.disabled = true;
-        lessChannelLevelButton.disabled = true;
-        silenceButton.disabled = true;
         settings = {
             volumenLevel: 3,
             channelLevel: 7
         };
-        updateButtons();
+    } else {
+        powerButton.innerText = 'Encender';
+        settings = {
+            volumenLevel: 0,
+            channelLevel: 0
+        };
     }
+    updateButtons();
+    printStatus();
 });
 
 moreVolumenLevelButton.addEventListener('click', () => {
-    settings.volumenLevel= settings.volumenLevel < 10 ? settings.volumenLevel + 1 : 'Volumen al máximo';
-    volumenLevel = settings.volumenLevel;
+    if(powerOn && !silence && settings.volumenLevel < 10) {
+        settings.volumenLevel += 1;
+        console.log(`Volumen aumentado a: ${settings.volumenLevel}`);
+    } else {
+        console.log('No se puede aumentar el volumen');
+    }
     updateButtons();
+    printStatus();
 });
 
 lessVolumenLevelButton.addEventListener('click',() => {
-    settings.volumenLevel = settings.volumenLevel < 10 ? settings.volumenLevel - 1 : settings.volumenLevel;
-    volumenLevel = settings.volumenLevel;
+    if(powerOn && !silence && settings.volumenLevel > 1) {
+        settings.volumenLevel -= 1;
+        console.log(`Volumen disminuido a: ${settings.volumenLevel}`);
+    } else {
+        console.log('No se puede disminuir el volumen');
+    }
     updateButtons();
+    printStatus();
 });
 
 moreChannelLevelButton.addEventListener('click', () => {
-    settings.channelLevel = settings.channelLevel < 100 ? settings.channelLevel + 1 : 'Canal al máximo';
-    channelLevel = settings.channelLevel;
+    if(powerOn && settings.channelLevel < 100) {
+        settings.channelLevel += 1;
+        console.log(`Canal aumentado a: ${settings.channelLevel}`);
+    } else {
+        console.log('No se puede aumentar el canal');
+    }
     updateButtons();
+    printStatus();
 });
 
 lessChannelLevelButton.addEventListener('click',() => {
-    settings.channelLevel = settings.channelLevel < 100 ? settings.channelLevel - 1 : settings.channelLevel;
-    channelLevel = settings.channelLevel;
+    if(powerOn && settings.channelLevel > 1) {
+        settings.channelLevel -= 1;
+        console.log(`Canal disminuido a: ${settings.channelLevel}`);
+    } else {
+        console.log('No se puede disminuir el canal');
+    }
     updateButtons();
+    printStatus();
 });
 
 silenceButton.addEventListener('click', () => {
-    silence = !silence;
-
-    if (silence) {
-        silenceButton.innerText = 'Silencio';
-        volumenButton.disabled = false;
-        channelButton.disabled = false;
-        moreVolumenLevelButton.disabled = false;
-        lessVolumenLevelButton.disabled = false;
-        moreChannelLevelButton.disabled = false;
-        lessChannelLevelButton.disabled = false;
-        silenceButton.disabled = true;
-        silence = true;
-        settings = {
-            volumenLevel: settings.volumenLevel,
-            channelLevel: settings.channelLevel
+     if (powerOn) {
+        silence = !silence;
+        if (silence) {
+            volumenAnterior = settings.volumenLevel;
+            settings.volumenLevel = 0;
+            console.log('Silencio activado');
+        } else {
+            settings.volumenLevel = volumenAnterior; 
+            console.log('Silencio desactivado');
         }
-        updateButtons();
     } else {
-        silenceButton.innerText = 'Silencio';
-        volumenButton.disabled = true;
-        channelButton.disabled = true;
-        moreVolumenLevelButton.disabled = true;
-        lessVolumenLevelButton.disabled = true;
-        moreChannelLevelButton.disabled = true;
-        lessChannelLevelButton.disabled = true;
-        silenceButton.disabled = true;
-        silence = false;
-        settings = {
-            volumenLevel: 0,
-            channelLevel: settings.channelLevel
-        };
-        updateButtons();
+        console.log('Televisor apagado');
     }
+        updateButtons();
+        printStatus();
 });
 
